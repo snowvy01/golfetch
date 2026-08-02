6e91eba370b78dbf6a4ab8f4fac3d66bb5115852
Snowvy <a40166895@gmail.com>
2026-08-02 20:33:38 +0500

    Added usage percentage, usage in GiB and total GiB.

--- modules/machine/filesystems.go
+++ modules/machine/filesystems.go
@@ -4,12 +4,16 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"syscall"
 )
 
 type FSInfo struct {
 	Device     string
 	MountPoint string
 	FSType     string
+	Used       float64
+	Total      float64
+	Percentage float64
 }
 
 func GetFSSystems() []FSInfo {
@@ -47,11 +51,29 @@ func GetFSSystems() []FSInfo {
 			continue
 		}
 
+		var stat syscall.Statfs_t
+		if err := syscall.Statfs(mountedTo, &stat); err != nil {
+			continue
+		}
+
+		const gib = 1024 * 1024 * 1024
+		totalBytes := stat.Blocks * uint64(stat.Bsize)
+		freeBytes := stat.Bavail * uint64(stat.Bsize)
+		usedBytes := totalBytes - freeBytes
+
+		var percent float64
+		if totalBytes > 0 {
+			percent = (float64(usedBytes) / float64(totalBytes)) * 100
+		}
+
 		seenDevices[device] = true
 		mounts = append(mounts, FSInfo{
 			Device:     device,
 			MountPoint: mountedTo,
 			FSType:     fstype,
+			Used:       float64(usedBytes) / gib,
+			Total:      float64(totalBytes) / gib,
+			Percentage: percent,
 		})
 	}
 	if scanner.Err() != nil {
